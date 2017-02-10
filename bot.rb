fe5f402e081e8cabe5abd15eefb84a8117c313ae
aiden <aiden.scott@pay4later.com>
2017-02-10 19:08:02 +0000

    working on async html lib

--- bot.rb
+++ bot.rb
@@ -7,6 +7,11 @@ require 'net/http'
 
 require 'uri'
 
+require 'unirest'
+
+
+base_url = "http://6c56750d.ngrok.io"
+
 # if we're on our local machine, we want to test our bot via shell, but when on
 # heroku, deploy the actual slackbot.
 # 
@@ -24,58 +29,37 @@ log = ChatAdapter.log
 # Do this thing in this block each time the bot hears a message:
 bot.on_message do |message, info|
   # ignore all messages not directed to this bot
-  if message.start_with? 'voice'
-    command, announcement = message.split(': ', 2)
+  case
+    when message.start_with?('voice')
+      command, announcement = message.split(': ', 2)
+    when message.include?('php fatal errors')
+      command = 'error'
+    else
+      next
   end
-  #elsif message.include? 'php fatal errors'
-    #command = 'error'
-    #if message.include? 'secure_error_log'
-      #errorlocation = 'secure_error_log'
-      #announcement = 'Emergency! Emergency! Fatal error detected on secure'
-    #elsif message.include? 'test_error_log'
-      #errorlocation = 'test_error_log'
-      #announcement = 'Oops... Not to worry too much, but an error was detected on test'
-    #elsif message.include? 'demo_error_log'
-      #errorlocation = 'demo_error_log'
-      #announcement = 'Excuse me, but I have detected an error on demo'
-    #elsif message.include? 'staging_error_log'
-      #errorlocation = 'staging_error_log'
-      #announcement = 'We do not care, but I just found an error on staging. You can ignore me'
-    #else
-      #errorlocation = 'unknown'
-      #announcement = 'I apologise, but I need to make you aware that I have detected an error but I do not know what it is'
-    #end
-  #end
 
-  if command == 'voice'
-    uri = URI.parse("http://6c56750d.ngrok.io/Office/say/#{URI.escape(announcement)}/Brian/60")
-    Net::HTTP.get_response(uri)
-    "@#{info[:user]}: saying  #{announcement}"
+  announcement = URI.escape(announcement)
+  case command
+    when 'voice'
+      uri = "#{base_url}/Office/say/#{announcement}/Brian/80"
+    when 'error'
+      # uri = URI.parse("#{base_url}/#{command}/#{errorlocation}/#{announcement}")
   end
 
 
-  #if command == 'error'
-   # uri = URI.parse("http://6c56750d.ngrok.io/#{command}/#{errorlocation}/#{URI.escape(announcement)}")
-#  else
- #   uri = URI.parse("http://6c56750d.ngrok.io/#{command}/#{URI.escape(announcement)}")
-  #end
-  
-
-
-  # Conditionally send a direct message to the person saying whisper
-  if message == 'echobot: whisper'
-    # log some info - useful when something doesn't work as expected
-    log.debug("Someone whispered! #{info}")
-    # and send the actual message
-    bot.direct_message(info[:user], "whisper-whisper")
-  end
-
-  # split the message in 2 to get what was actually said.
-  
-  # answer the query!
-  # this bot simply echoes the message back
-  # "@#{info[:user]}: saying  #{announcement}"
+  Unirest.post(uri)
+  "@#{info[:user]}: saying  #{announcement}"
 end
+
+
+=begin
+if command == 'error'
+ d
+else
+  uri = URI.parse("http://6c56750d.ngrok.io/#{command}/#{URI.escape(announcement)}")
+end
+=end
+
 
 # actually start the bot
 bot.start!
